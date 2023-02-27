733dcfc0d4a2a161bc85ea3b13378f1ed7adc5be
Frank <frank@mytappr.com>
2023-02-27 16:41:11 +1000

    fix calculation

--- src/components/SalaryForm/utils.js
+++ src/components/SalaryForm/utils.js
@@ -4,11 +4,11 @@ export const taxCalculator = income => {
   } else if (income >= 18201 && income <= 37000) {
     return income * 0.19
   } else if (income >= 37001 && income <= 90000) {
-    return 3572 + income * 0.325
+    return 3572 + (income - 37000) * 0.325
   } else if (income >= 90001 && income <= 180000) {
-    return 20797 + income * 0.37
+    return 20797 + (income - 90000) * 0.37
   } else {
-    return 54097 + income * 0.45
+    return 54097 + (income - 180000) * 0.45
   }
 }
 
